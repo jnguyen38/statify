acdb4240d6b2756eb7a4f76db629413086c96404
jnguyen38 <jnguyen8831@gmail.com>
2022-07-30 17:13:18 -0400

    topTracks total data check, added options for topTrack API call

--- src/js/UserEndpoint.js
+++ src/js/UserEndpoint.js
@@ -69,27 +69,27 @@ function Dashboard(props) {
     useEffect(() => {
         if (!accessToken) return
         spotifyApi.setAccessToken(accessToken)
-        spotifyApi.getMyTopTracks()
+        spotifyApi.getMyTopTracks({time_range: "short_term", limit: 50})
             .then(data => {
                 console.log(data)
-                setTopTracks(
-                    data.body.items.map(track => {
-                        const smallestAlbumImage = track.album.images.reduce(
-                            (smallest, image) => {
-                                if (image.height < smallest.height) return image
-                                return smallest
-                            },
-                            track.album.images[0]
-                        )
+                let tracks = [];
+                data.body.items.map(track => {
+                    const smallestAlbumImage = track.album.images.reduce(
+                        (smallest, image) => {
+                            if (image.height < smallest.height) return image
+                            return smallest
+                        },
+                        track.album.images[0]
+                    )
 
-                        return {
-                            artist: track.artists[0].name,
-                            title: track.name,
-                            uri: track.uri,
-                            albumUrl: smallestAlbumImage.url,
-                        }
+                    tracks.push({
+                        artist: track.artists[0].name,
+                        title: track.name,
+                        uri: track.uri,
+                        albumUrl: smallestAlbumImage.url,
                     })
-                )
+                })
+                setTopTracks(tracks);
             }).catch(err => {
                 console.log(err);
         });
